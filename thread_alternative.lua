46328781640c3993f77bcb8db8ae06c6cf8af570
JayOHx <119745695+Jay0Hx@users.noreply.github.com>
2024-01-02 17:18:26 +0000

    removed examples, pointless, just going with a slick approach
    simply copy and paste this for ragdoll, G key.

--- thread_alternative.lua
+++ thread_alternative.lua
@@ -1,16 +1,3 @@
--- Where ever using threads you can replace them as per the example below. (Much more server friendly)
-
--- Thread Example:
-Citizen.CreateThread(function() 	
-    while true do 		
-        Citizen.Wait(0) 		
-        if IsControlPressed(1, 47 --[[ "G" key ]]) then 	
-            SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, true, true, false) 
-        end 	
-    end 
-end)
-
--- Replacement example using KeyMapping:
 RegisterKeyMapping('lakeside_ragdoll', 'Go all floppy and pass out', 'keyboard', 'G')
 RegisterCommand('lakeside_ragdoll', function()
     SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, true, true, false)
