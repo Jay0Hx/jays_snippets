e7ecc91da5310344cb5133984e6424b03a8460c8
JayOHx <119745695+Jay0Hx@users.noreply.github.com>
2024-01-02 17:24:42 +0000

    Quick Update

--- hands_up.lua
+++ hands_up.lua
@@ -1,21 +1,17 @@
-Citizen.CreateThread(function()
-    local dict = "missminuteman_1ig_2"
-    
-	RequestAnimDict(dict)
-	while not HasAnimDictLoaded(dict) do
+RegisterKeyMapping('lakeside_handsUp', 'Surrender, hands up!', 'keyboard', 'X')
+RegisterCommand('lakeside_handsUp', function()				
+	RequestAnimDict("missminuteman_1ig_2")
+	while not HasAnimDictLoaded("missminuteman_1ig_2") do
 		Citizen.Wait(100)
 	end
-    local handsup = false
-	while true do
-		Citizen.Wait(0)
-		if IsControlJustPressed(1, 323) then --Start holding X
-            if not handsup then
-                TaskPlayAnim(GetPlayerPed(-1), dict, "handsup_enter", 8.0, 8.0, -1, 50, 0, false, false, false)
-                handsup = true
-            else
-                handsup = false
-                ClearPedTasks(GetPlayerPed(-1))
-            end
-        end
-    end
+
+	if not handsUp then
+		TaskPlayAnim(GetPlayerPed(-1), dict, "handsup_enter", 8.0, 8.0, -1, 50, 0, false, false, false)
+		handsUp = true
+	else
+		handsup = false
+		ClearPedTasks(GetPlayerPed(-1))
+	end
 end)
+
+-- I have not tested if this works yet, i presume it does but, if not, let me know on discord.
