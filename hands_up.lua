bf33b34d36aa10ed90be98d9237b73affc2b0556
JayOHx <119745695+Jay0Hx@users.noreply.github.com>
2024-01-02 17:19:33 +0000

    base

--- hands_up.lua
+++ hands_up.lua
@@ -1,10 +1,21 @@
-Citizen.CreateThread(function() 	
-    while true do 		
-        Citizen.Wait(0) 		
-        if IsControlPressed(1, 47 --[[ "G" key ]]) then 	
-            SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, true, true, false) 
-        end 	
-    end 
+Citizen.CreateThread(function()
+    local dict = "missminuteman_1ig_2"
+    
+	RequestAnimDict(dict)
+	while not HasAnimDictLoaded(dict) do
+		Citizen.Wait(100)
+	end
+    local handsup = false
+	while true do
+		Citizen.Wait(0)
+		if IsControlJustPressed(1, 323) then --Start holding X
+            if not handsup then
+                TaskPlayAnim(GetPlayerPed(-1), dict, "handsup_enter", 8.0, 8.0, -1, 50, 0, false, false, false)
+                handsup = true
+            else
+                handsup = false
+                ClearPedTasks(GetPlayerPed(-1))
+            end
+        end
+    end
 end)
--- Made by STUN#0007
---If you want a different key bind go to https://docs.fivem.net/docs/game-references/controls/#controls jump G for the ozark days
